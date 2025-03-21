88aa0852d5959c66a0856e0286d2b539afacdfe4
WackStr <waqasrazajafri@gmail.com>
2025-03-22 00:52:17 +0500

    Initial work on login flow

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import LoginForm from './components/LoginForm.vue';
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="PSX Tracker" />
+  <LoginForm/>
 </template>
 
 <style scoped>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,55 @@
+<script setup>
+import {ref} from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+const handleLogin = () => {
+    console.log("login attempted");
+}
+</script>
+
+<template>
+        <div class="login-container">
+            <h2>Login</h2>
+        <label>
+            Username:
+            <input type="text" v-model="username" />
+        </label>
+        <br />
+        <label>
+            Password:
+            <input type="password" v-model="password" />
+        </label>
+        <br />
+        <button @click="handleLogin">Login</button>
+    </div>
+</template>
+
+<style scoped>
+.login-container {
+    max-width: 300px;
+    margin: 40px auto;
+    text-align: left;
+}
+.login-container h2 {
+    text-align: center;
+    margin-bottom: 1rem;
+}
+.login-container label {
+    display: block;
+    margin-bottom: 0.5rem;
+}
+.login-container input {
+    width: 100%;
+    padding: 0.5 rem;
+    margin-top: 0.25rem;
+    box-sizing: border-box;
+}
+.login-container button{
+    margin-top: 1rem;
+    width: 100%;
+    padding: 0.5rem;
+    cursor: pointer;
+}
+</style>
